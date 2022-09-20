53a6139a98bc80c234435f289540ac97e34e2843
threebarber <dvtdropshot@gmail.com>
2022-09-19 21:31:29 -0400

    more logic, task info within project container

--- scripts.js
+++ scripts.js
@@ -37,8 +37,36 @@ const project = function(title, toDoList,){
 
 const utils = (() => {
 
-    const log = (msg) => console.log(`${date.toLocaleTimeString()} - ${msg}`)
+    const log = (msg) => console.log(`${date.toLocaleTimeString()} - ${msg}`);
 
+
+
+    const createTaskDiv = function (taskList){
+       
+        var taskContainer = document.createElement("div");
+        taskContainer.classList.add("taskContainer");
+       
+        taskList.forEach(task => {
+
+            var taskDiv = document.createElement("div");
+            taskDiv.classList.add("taskDiv");
+
+            var taskName = document.createElement("h4");
+            taskName.innerText = task.title;
+
+            var taskDescription = document.createElement("p");
+            taskDescription.innerText = task.description;
+
+
+            taskDiv.appendChild(taskName);
+            taskDiv.appendChild(taskDescription);
+
+            taskContainer.appendChild(taskDiv);
+        });
+
+
+        return taskContainer;
+    }
 
 
     const displayProjects = function (projectList) {
@@ -48,19 +76,23 @@ const utils = (() => {
 
         projectList.forEach(project => {
             
-            utils.log(`Displaying project ${project.title}`)
+            utils.log(`Displaying project ${project.title}`);
 
             var projectDiv = document.createElement("div");
             projectDiv.classList.add("projectDiv");
-            projectDiv.classList.add("card");
 
 
             var projName = document.createElement("h2");
-            projName.classList.add("card-title");
             projName.innerText = project.title;
             
 
             projectDiv.appendChild(projName);
+
+
+            var taskContainer = utils.createTaskDiv(project.toDoList);
+
+            projectDiv.appendChild(taskContainer);
+
 
             projContainer.appendChild(projectDiv);
 
@@ -71,17 +103,23 @@ const utils = (() => {
     return {
 
         log,
-        displayProjects
+        displayProjects,
+        createTaskDiv,
     };
 
 })();
 
 
 exampleTask = new task("take out trash", "make sure trash is taken out","9-20-22","low","example","notes: it is really smelly right now");
+exampleTaskTwo = new task("take out trash #2", "make sure trash is taken out","9-20-22","low","example","notes: it is really smelly right now");
+exampleTaskThree = new task("take out trash #3", "make sure trash is taken out","9-20-22","low","example","notes: it is really smelly right now");
 
 exampleTaskList = [];
 
 exampleTaskList.push(exampleTask);
+exampleTaskList.push(exampleTaskTwo);
+exampleTaskList.push(exampleTaskThree);
+
 
 
 exampleProj = new project("example",exampleTaskList);
